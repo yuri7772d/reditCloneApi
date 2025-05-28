ed7312912d6a5dfb3edc50687ff51c6c6f841e65
Nattapong chaisisa <yuri7772d@gmail.com>
2025-05-28 11:59:25 +0700

    test interface

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type (
+	item interface {
+		use()
+	}
+	sword struct{}
+)
+
+func (s *sword) use() {
+	fmt.Printf("Use Sword!!")
+
+}
+
+func useItem(i item) {
+	i.use()
+
+}
+
 func main() {
+	Sword := sword{}
+	useItem(Sword)
 	fmt.Printf("hello world!")
 }
